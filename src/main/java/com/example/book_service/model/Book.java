dfd6dbc8116740c8f3f658714b457e6bb0206996
Syed1012 <syedmusaib66@gmail.com>
2024-10-24 13:45:45 +0530

    small change with the credentials in the secrets password (github) proceed with test.

--- src/main/java/com/example/book_service/model/Book.java
+++ src/main/java/com/example/book_service/model/Book.java
@@ -2,7 +2,6 @@ package com.example.book_service.model;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
